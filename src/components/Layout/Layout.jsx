15f73e6ccfc12ea1ebe4e0d2aaeb1cb8d97bfa48
Jarrison Cano <jarrison.cano@hotmail.com>
2021-05-22 13:54:03 -0500

    Created a Layout component and implement the styles files

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,5 @@
+import { LayoutStyled } from './styles'
+
+export const Layout = ({ children }) => {
+	return <LayoutStyled>{children}</LayoutStyled>
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import './styles/styles.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import './styles/normalize.css'
 
 ReactDOM.render(
 	<React.StrictMode>
