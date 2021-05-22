08fdf059335c723ecf557c7ade42537caf0dd080
Jarrison Cano <jarrison.cano@hotmail.com>
2021-05-22 13:51:38 -0500

    Added style settings and background to the page

---
+++ src/components/Layout/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const LayoutStyled = styled.div`
+	align-items: center;
+	flex-direction: column;
+	color: white;
+	display: flex;
+
+	margin: 0 auto;
+	max-width: 1300px;
+	min-height: 100vh;
+
+	h1 {
+		font-size: 4rem;
+		margin: 0;
+	}
+	p {
+		background-color: #05caa7;
+		border: 2px solid #008c6f;
+		border-radius: 2rem;
+		font-size: 1.3rem;
+
+		margin-top: 1rem;
+		padding: 0.3rem 2rem;
+	}
+`
