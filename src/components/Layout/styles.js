04fb57fd5de284eeedf72435b38eeced109daaef
Jarrison Cano <jarrison.cano@hotmail.com>
2021-06-24 18:47:54 -0500

    :lipstick: style: fix bug in the layout

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -1,64 +1,66 @@
 import styled from 'styled-components'
 
 export const LayoutStyled = styled.div`
-  align-items: center;
   background-color: ${(props) => (props.t ? '#494898' : 'white')};
   background-image: radial-gradient(#7674df 1px, transparent 0);
   background-position: 0 0, 20px 20px;
-
   background-size: 40px 40px;
+
   color: ${(props) => (props.t ? 'white' : '#494898')};
-  display: flex;
-  flex-direction: column;
-
   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  justify-content: space-between;
-  margin: 0 auto;
-  max-width: 1300px;
 
-  min-height: 100vh;
-
-  h1 {
-    font-size: 4rem;
-    margin: 0;
-    text-align: center;
-
-    span {
-      color: #c3c1d9;
-    }
-
-    @media (max-width: 850px) {
-      font-size: 3.8rem;
-      text-align: center;
-    }
-  }
-
-  .date {
-    background-color: #05caa7;
-    border: 2px solid #008c6f;
-    border-radius: 2rem;
-    box-shadow: 0rem 0rem 0.7rem black;
-
-    color: white;
+  .container {
+    align-items: center;
     display: flex;
+    flex-direction: column;
     justify-content: space-between;
-    font-size: 1.3rem;
-    height: 1.7rem;
 
-    margin-top: 1rem;
-    padding: 0.3rem 1rem;
-    text-align: center;
-    white-space: nowrap;
+    margin: 0 auto;
+    max-width: 1300px;
+    min-height: 100vh;
 
-    width: 8.5rem;
+    h1 {
+      font-size: 4rem;
+      margin: 0;
+      text-align: center;
 
-    .iconTheme {
-      cursor: pointer;
-      margin-left: 0.5rem;
-      width: 2.5rem;
+      span {
+        color: #c3c1d9;
+      }
 
-      path {
-        fill: ${(props) => (!props.t ? '#494898' : 'white')};
+      @media (max-width: 850px) {
+        font-size: 3.8rem;
+        text-align: center;
+      }
+    }
+
+    .date {
+      background-color: #05caa7;
+      border: 2px solid #008c6f;
+      border-radius: 2rem;
+      box-shadow: 0rem 0rem 0.7rem black;
+
+      color: white;
+      display: flex;
+      justify-content: space-between;
+      font-size: 1.3rem;
+      height: 1.7rem;
+
+      margin-top: 1rem;
+      padding: 0.3rem 1rem;
+      text-align: center;
+      white-space: nowrap;
+
+      width: 8.5rem;
+
+      .iconTheme {
+        cursor: pointer;
+        margin-left: 0.5rem;
+        width: 2.5rem;
+
+        path {
+          fill: ${(props) => (!props.t ? '#494898' : 'white')};
+        }
       }
     }
   }
